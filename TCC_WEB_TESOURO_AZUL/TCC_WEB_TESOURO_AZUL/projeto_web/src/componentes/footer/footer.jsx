fd2ac8d26e52f81d512b64a0fd48e17d49e66cd4
BEaew0 <beatrizsouzasscoli@gmail.com>
2025-05-03 14:41:10 -0300

    meme

---
+++ TCC_WEB_TESOURO_AZUL/TCC_WEB_TESOURO_AZUL/projeto_web/src/componentes/footer/footer.jsx
@@ -0,0 +1,27 @@
+export default function Header(){
+
+    return (
+    <div id="pag-conteudo">
+        <div class="overlay"></div> 
+        <div id="header-container">
+            <header>
+                <a href="pag_principal.html">
+                </a>
+  
+                <div class="itens_esquerda">
+                    <a href="download.html" id="itens">Download</a>
+                    <a id="itens" href="">Planos</a>
+                    <a href="#" id="itens">Desenvolvedores</a>
+                </div>
+  
+                <div class="itens_direita">
+                    <button id="btn_tema">
+                        <i class="fa-solid fa-sun" id="icon_tema"></i>
+                    </button>
+                    <a id="itens" href="login.html">Logar</a>
+                    <a id="itens" href="cadastro.html">Cadastrar</a>
+                </div>
+            </header>
+        </div>
+  </div>)
+  }
---
+++ TCC_WEB_TESOURO_AZUL/aula_react_ETEC-Isabella/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className="BOMBA">
+      <h1>Home</h1>
+      <p>Essa é a página inicial</p>
+      <p>Essa é a página inicial</p>
+    </div>
+  );
+}
